b00f1bfec7bc8051b07d76de7fabbeb2f1e6a23b
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2024-10-12 12:23:06 -0400

    proj1.1 done?

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import logo from '../assets/react.svg'
+
 export default function Navbar() {
-    return <h1>Navbar goes here</h1>
+    return (
+        <nav className='nav'>
+            <img className='nav-logo-img' src={logo} alt='' />
+            <h3 className='nav-logo-title'>ReactFacts</h3>
+            <h4 className='nav-desc'>React Course - Project 1</h4>
+        </nav>
+    )
 }
